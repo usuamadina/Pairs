af0bcacaf212f6f02c8aa9815da50c4345ff04fd
Usua Madina <u.madina.i@gmail.com>
2017-07-08 12:18:37 +0200

    Connecting the app with Google Play Games

--- app/src/main/java/com/example/pairs/Game.java
+++ app/src/main/java/com/example/pairs/Game.java
@@ -1,10 +1,13 @@
 package com.example.pairs;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
  * Created by usuwi on 08/07/2017.
  */
 
 public class Game {
+    public static GoogleApiClient mGoogleApiClient;
     public static int ROWS = -1;
     public static int COLUMNS = -1;
     public static int[][] boxes;
--- app/src/main/java/com/example/pairs/Menu.java
+++ app/src/main/java/com/example/pairs/Menu.java
@@ -2,24 +2,51 @@ package com.example.pairs;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+
 import java.util.ArrayList;
 import java.util.Random;
+
+import static com.example.pairs.Game.mGoogleApiClient;
 
 /**
  * Created by usuwi on 08/07/2017.
  */
 
-public class Menu extends Activity {
+public class Menu extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private Button btnPlay;
+    private static final int RC_SIGN_IN = 9001;
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInflow = true;
+    private boolean mSignInClicked = false;
+    private com.google.android.gms.common.SignInButton btnConnect;
+    private Button btnDisconnect;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
         btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnConnect = (com.google.android.gms.common.SignInButton) findViewById(R.id.sign_in_button);
+        btnConnect.setOnClickListener(btnConnect_Click);
+        btnDisconnect = (Button) findViewById(R.id.sign_out_button);
+        btnDisconnect.setOnClickListener(btnDisconnect_Click);
+        Game.mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(Games.API).addScope(Games.SCOPE_GAMES).build();
+        SharedPreferences prefs = getSharedPreferences("Parejas", MODE_PRIVATE);
+        int conectado = prefs.getInt("conectado", 0);
+        if (conectado != 0) {
+            Game.mGoogleApiClient.connect();
+        }
+
     }
 
     public void btnPlay_Click(View v) {
@@ -52,5 +79,71 @@ public class Menu extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+        findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure) {
+            return;
+        }
+        if (mSignInClicked) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+            if (!BaseGameUtils.resolveConnectionFailure(this, mGoogleApiClient, connectionResult, RC_SIGN_IN, "Hubo un error al conectar, por favor, inténtalo más tarde.")) {
+                mResolvingConnectionFailure = false;
+            }
+
+        }
+    }
+
+    View.OnClickListener btnConnect_Click = new View.OnClickListener() {
+        public void onClick(View v) {
+            mSignInClicked = true;
+            mGoogleApiClient.connect();
+        }
+    };
+    View.OnClickListener btnDisconnect_Click = new View.OnClickListener() {
+        public void onClick(View v) {
+            mSignInClicked = false;
+            Games.signOut(mGoogleApiClient);
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+            SharedPreferences.Editor editor = getSharedPreferences("Parejas", MODE_PRIVATE).edit();
+            editor.putInt("conectado", 0);
+            editor.commit();
+        }
+    };
+
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        super.onActivityResult(requestCode, responseCode, intent);
+        switch (requestCode) {
+            case RC_SIGN_IN:
+                mSignInClicked = false;
+                mResolvingConnectionFailure = false;
+                if (responseCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                    SharedPreferences.Editor editor = getSharedPreferences("Parejas", MODE_PRIVATE).edit();
+                    editor.putInt("conectado", 1);
+                    editor.commit();
+                } else {
+                    BaseGameUtils.showActivityResultError(this, requestCode, responseCode, R.string.unknown_error);
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, responseCode, intent);
     }
 }
